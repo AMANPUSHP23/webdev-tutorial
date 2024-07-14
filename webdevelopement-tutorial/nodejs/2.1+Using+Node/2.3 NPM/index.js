bcb8ba50a6cb189cc8ccdc1c22e5460ec4c824a8
AMANPUSHP23 <amanpushp2001@gmail.com>
2024-07-14 10:10:02 +0530

    nodejs added

---
+++ webdevelopement-tutorial/nodejs/2.1+Using+Node/2.3 NPM/index.js
@@ -0,0 +1,17 @@
+//var generateName=require("sillyname");
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+// import superheroes from "superheroes";
+
+// const name = superheroes.randomSuperhero();
+
+//  console.log(`I am ${name}!`);
+
+
+//  import {randomSuperhero} from 'superheroes';
+
+// randomSuperhero();
